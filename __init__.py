5b034e07cbace1ab67927fce1565473d443893b7
Denis Rouzaud <denis.rouzaud@gmail.com>
2013-06-26 17:14:47 +0200

    remove old stuff

--- __init__.py
+++ __init__.py
@@ -6,16 +6,7 @@ Link It
 QGIS module
 
 """
-def name():
-    return "Link It"
-def description():
-    return "To link a feature to another. By clicking on map, the plugin will then automatically save the feature's ID in the appropriate field defined by the user. [This plugin requires ItemBrowser]"
-def version():
-    return "Version 2.0"
-def icon():
-    return "icons/linkit.png"
-def qgisMinimumVersion():
-    return "1.8"
+
 def classFactory(iface):
     from linkit import LinkIt
     return LinkIt(iface)
