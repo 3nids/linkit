af39900db46c2b906ea8f20429e1e3efbc308b2c
Denis Rouzaud <denis.rouzaud@gmail.com>
2013-09-02 10:44:50 +0200

    added dock position preference

--- core/mysettings.py
+++ core/mysettings.py
@@ -7,3 +7,5 @@ class MySettings(SettingManager):
     def __init__(self):
         SettingManager.__init__(self, pluginName)
         self.addSetting("drawButton", "bool", "global", True)
+        self.addSetting("dockArea", "integer", "global", 0)
+
--- linkit.py
+++ linkit.py
@@ -11,9 +11,11 @@ from PyQt4.QtCore import QUrl, Qt
 from PyQt4.QtGui import QIcon, QAction, QDesktopServices
 from qgis.core import QgsMapLayerRegistry, QgsFeature, QgsFeatureRequest, QgsProject
 
-from core.link import Link, getLink
+from core.mysettings import MySettings
+from core.link import getLink
 from gui.linkmanagerdialog import LinkManagerDialog
 from gui.linkerdock import LinkerDock
+from gui.mysettingsdialog import MySettingsDialog
 
 import resources
 
@@ -28,6 +30,11 @@ class LinkIt():
         self.linkManagerAction = QAction(QIcon(":/plugins/linkit/icons/connect.png"), "Links manager", self.iface.mainWindow())
         self.linkManagerAction.triggered.connect(self.linkManagerDialog)
         self.iface.addPluginToMenu("&Link It", self.linkManagerAction)
+        # settings
+        self.settingsAction = QAction(QIcon(":/plugins/linkit/icons/settings.svg"), "Settings",
+                                      self.iface.mainWindow())
+        self.settingsAction.triggered.connect(self.showSettings)
+        self.iface.addPluginToMenu("&Link It", self.settingsAction)
         # help
         self.helpAction = QAction(QIcon(":/plugins/linkit/icons/help.png"), "Help", self.iface.mainWindow())
         self.helpAction.triggered.connect(lambda: QDesktopServices().openUrl(QUrl("http://3nids.github.io/linkit")))
@@ -35,6 +42,7 @@ class LinkIt():
                   
     def unload(self):
         self.iface.removePluginMenu("&Link It", self.linkManagerAction)
+        self.iface.removePluginMenu("&Link It", self.settingsAction)
         self.iface.removePluginMenu("&Link It", self.helpAction)
 
     def linkManagerDialog(self):
@@ -57,5 +65,11 @@ class LinkIt():
         if destinationLayer.getFeatures(QgsFeatureRequest().setFilterFid(featureId)).nextFeature(f) is False:
             return
         self.linkerDock = LinkerDock(self.iface.mapCanvas(), destinationLayer, destinationField, sourceLayer, f)
-        self.iface.addDockWidget(Qt.LeftDockWidgetArea, self.linkerDock)
+        if MySettings().value("dockArea") == 1:
+            self.iface.addDockWidget(Qt.RightDockWidgetArea, self.linkerDock)
+        else:
+            self.iface.addDockWidget(Qt.LeftDockWidgetArea, self.linkerDock)
         destinationLayer.layerDeleted.connect(lambda: self.iface.removeDockWidget(self.linkerDock))
+
+    def showSettings(self):
+        MySettingsDialog().exec_()
